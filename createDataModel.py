c545041c1e10d695c223a54e430ae834e8169659
Dag Einar Monsen <me@dag.im>
2013-10-10 15:45:43 +0200

    Convert data delimiter from :: to tab character.

--- createDataModel.py
+++ createDataModel.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
 # script :: creating a datamodel that fits mahout from ratings.dat
 
-ratings_dat = open('../data/movielens-1m/ratings.dat', 'r')
 
-ratings_csv = open('../data/movielens-1m/ratings_without_timestamp.txt', 'w')
+
+ratings_dat = open('../data/movielens-1m/users.dat', 'r')
+ratings_csv = open('../data/movielens-1m/users.txt', 'w')
 
 for line in ratings_dat:
 	arr = line.split('::')
-	new_line = ','.join(arr[:3])+'\n';
+	new_line = '\t'.join(arr)
 
 	ratings_csv.write(new_line)
 
